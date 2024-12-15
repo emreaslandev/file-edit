2ea1217ac050345a02d623294cd905b26a80adee
emreaslandev <emreaslan.dev@gmail.com>
2024-12-16 02:35:48 +0300

    dosya adlandırma

---
+++ dosta-adlandırma.py
@@ -0,0 +1,24 @@
+import os
+
+# Kullanıcıdan klasör yolunu al
+folder_path = '/Users/emreaslan/Downloads/agdas'
+
+# Klasördeki tüm dosyaları al
+files = os.listdir(folder_path)
+
+# Dosyaları sıralama
+files.sort()
+
+# Dosyaların adlarını değiştirme
+for index, file in enumerate(files):
+    # Dosya yolunu oluştur
+    old_file_path = os.path.join(folder_path, file)
+    
+    # Yeni dosya adını oluştur (000001, 000002, vb.)
+    new_file_name = f"{str(index + 1).zfill(6)}{os.path.splitext(file)[1]}"
+    new_file_path = os.path.join(folder_path, new_file_name)
+    
+    # Dosyayı yeniden adlandır
+    os.rename(old_file_path, new_file_path)
+
+print("Dosya isimleri başarıyla değiştirildi.")
