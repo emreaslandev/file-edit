cfd4617dad27514c1d1ccf32f6d07fa6ff724359
emreaslandev <emreaslan.dev@gmail.com>
2025-05-27 14:28:53 +0300

    yol adına göre düzenleme ve hata giderimi yapıldı.

--- file-rename-edit.py
+++ file-rename-edit.py
@@ -3,22 +3,20 @@ import os
 # Kullanıcıdan klasör yolunu al
 folder_path = input("Lütfen klasör yolunu girin: ")
 
-# Klasördeki tüm dosyaları al
-files = os.listdir(folder_path)
+# Sadece dosya olanları ve gizli olmayanları al
+files = [
+    f for f in os.listdir(folder_path)
+    if os.path.isfile(os.path.join(folder_path, f)) and not f.startswith('.')
+]
 
 # Dosyaları sıralama
 files.sort()
 
 # Dosyaların adlarını değiştirme
 for index, file in enumerate(files):
-    # Dosya yolunu oluştur
     old_file_path = os.path.join(folder_path, file)
-    
-    # Yeni dosya adını oluştur (000001, 000002, vb.)
     new_file_name = f"{str(index + 1).zfill(6)}{os.path.splitext(file)[1]}"
     new_file_path = os.path.join(folder_path, new_file_name)
-    
-    # Dosyayı yeniden adlandır
     os.rename(old_file_path, new_file_path)
 
 print("Dosya isimleri başarıyla değiştirildi.")
